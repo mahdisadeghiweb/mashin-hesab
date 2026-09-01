1282c78b459c10bf9b6b23089811abb6cb96ba9d
mahdisadeghiweb <mahdi.sadeghi.web@gmail.com>
2026-09-01 13:04:59 +0330

    add mashin hesab to github

---
+++ asets/statements.js
@@ -0,0 +1,18 @@
+//var X = 13;
+/*
+var X = 13;
+*/
+// document.write(5)
+// document.getElementById("demo").innerHTML = "سلام من یک خروجی هستم!";
+// // window.alert("سلام");
+// //alert("سلام 2");
+// console.log("سلام کنسول از ");
+
+
+
+
+
+
+
+
+
